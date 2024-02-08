a9ab9c7212f9a442d9b2a9350f65da960876b2a7
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-02-08 16:44:05 +0000

    FIX

--- hello_world.c
+++ hello_world.c
@@ -2,7 +2,7 @@
 
 int  main()
 {
-    printf("Hello. My email address is:\n jeong-hyun.kim@student.manchester.ac.uk");
+    printf("Hello. My email address is:\njeong-hyun.kim@student.manchester.ac.uk");
 
     return 0;
 }
