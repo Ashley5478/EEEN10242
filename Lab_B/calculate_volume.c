bba1f951ea54d314e502466120f91e54d0a10109
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-02-15 16:23:38 +0000

    Created Task B

---
+++ Lab_B/calculate_volume.c
@@ -0,0 +1,13 @@
+// Name goes here
+// 
+// Aim
+// Template file for code in Lab B
+// ----------------------------------------------------------------------------
+
+int main(void){
+    double h, l;
+    float w = 0.0f;
+	
+	
+    return 0;
+}
