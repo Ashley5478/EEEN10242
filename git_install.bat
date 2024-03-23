09a194e55734cc7f4ad6de88351f0c9eb5f4c521
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-03-23 15:13:04 +0100

    [BATCH] Commit changes via batch script

---
+++ git_install.bat
@@ -0,0 +1,3 @@
+git init
+git branch -M main
+git remote add origin https://github.com/Ashley5478/EEEN10242.git
---
+++ git_push.bat
@@ -0,0 +1,8 @@
+git remote add origin https://github.com/Ashley5478/EEEN10242.git
+git config --global user.name "Jeong-Hyun Kim"
+git config --global user.email "jeong-hyun.kim@student.manchester.ac.uk"
+git branch -M main
+git add -A
+git add -A
+git commit -m "[BATCH] Commit changes via batch script"
+git push -u origin main
