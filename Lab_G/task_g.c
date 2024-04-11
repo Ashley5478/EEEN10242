223136a13f02579a7f51b06d80042c91379fb79d
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-04-11 11:21:35 +0100

    update

---
+++ Lab_G/task_g.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task_g.h"
+
+int main(void) {
+
+    // Your code below here
+    
+    
+	
+
+
+    return 0;
+}
---
+++ Lab_G/task_g.h
@@ -0,0 +1,6 @@
+void find_max_less_than_10(const double a[], const int n, double *max) {
+    
+    // Your code below here
+    
+    
+}
