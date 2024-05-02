52892ce62e04b4a525d61affb73e7116689234ef
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-05-02 17:49:25 +0100

    [BATCH] Commit changes via batch script

--- Optional/Sandbox.c
+++ Optional/Sandbox.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 int main ()
 {
     int moves, move_code;
@@ -16,33 +17,31 @@ int main ()
     printf("Moves | Code | Direction\n");
     for(int i = 1; i <= moves; i++) {
         move_code = rand() % 8;
-        switch (move_code) {
-            case 0: printf("%5d |   %d | N\n", i , move_code);
+        switch(move_code) {
+            case 0: printf("%5d |    %d | N\n", i , move_code);
             break;
 
-            case 1: printf("%5d |   %d | NE\n", i , move_code);
+            case 1: printf("%5d |    %d | NE\n", i , move_code);
             break;
 
-            case 2: printf("%5d |   %d | E\n", i , move_code);
+            case 2: printf("%5d |    %d | E\n", i , move_code);
             break;
 
-            case 3: printf("%5d |   %d | SE\n", i , move_code);
+            case 3: printf("%5d |    %d | SE\n", i , move_code);
             break;
 
-            case 4: printf("%5d |   %d | S\n", i , move_code);
+            case 4: printf("%5d |    %d | S\n", i , move_code);
             break;
 
-            case 5: printf("%5d |   %d | SW\n", i , move_code);
+            case 5: printf("%5d |    %d | SW\n", i , move_code);
             break;
 
-            case 6: printf("%5d |   %d | W\n", i , move_code);
+            case 6: printf("%5d |    %d | W\n", i , move_code);
             break;
 
-            default: printf("%5d |   %d | NW\n", i , move_code);
+            default: printf("%5d |    %d | NW\n", i , move_code);
             break;
-        }
-
-    
+        }    
     }
     return 0;
 }
